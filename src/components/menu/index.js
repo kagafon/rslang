99a0b472435dd4762176b8d8cbbb8a830a59699f
iryna.blotskaya <charlie.the.unicorn.blbl@gmail.com>
2020-06-19 16:44:07 +0300

    feat: add transition to main-container

--- src/components/menu/index.js
+++ src/components/menu/index.js
@@ -3,6 +3,7 @@ import images from 'components/menu/backgroundImages';
 import router from 'components/Router/';
 class Menu {
   init() {
+    const container = document.querySelector('#main-container');
     const links = document.querySelector('#links');
     const navbar = document.querySelector('#navbar');
     const aLinks = [...links.querySelectorAll('a')];
@@ -15,7 +16,13 @@ class Menu {
       navbar.style.backgroundColor = `${
         images[event.target.dataset.link].color
       }`;
-      router.draw(`${event.target.dataset.link}`);
+      this.handler = () => {
+        router.draw(`${event.target.dataset.link}`);
+        container.style.opacity = '1';
+        container.removeEventListener('transitionend', this.handler);
+      };
+      container.style.opacity = '0';
+      container.addEventListener('transitionend', this.handler);
       event.target.classList.add('active');
     });
   }
