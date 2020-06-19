c27ba1bdda56271ab1b486cb0cd3ae15b4083432
iryna.blotskaya <charlie.the.unicorn.blbl@gmail.com>
2020-06-19 14:32:38 +0300

    add: router for links

--- src/components/menu/index.js
+++ src/components/menu/index.js
@@ -1,18 +1,23 @@
 // import { createElement } from 'helpers/dom';
-import images from 'components/menu/backgroundImages'
-
+import images from 'components/menu/backgroundImages';
+import router from 'components/Router/';
 class Menu {
   init() {
-   const links = document.querySelector('#links')
-   const navbar = document.querySelector('#navbar')
-   const aLinks = [...links.querySelectorAll('a')]
-   links.addEventListener('click', (event) => {
-    event.preventDefault()
-    aLinks.forEach(a => a.classList.remove('active'))
-    document.body.style.backgroundImage = `url(${images[event.target.dataset.link].image})`
-    navbar.style.backgroundColor = `${images[event.target.dataset.link].color}`
-    event.target.classList.add('active')
-   })
+    const links = document.querySelector('#links');
+    const navbar = document.querySelector('#navbar');
+    const aLinks = [...links.querySelectorAll('a')];
+    links.addEventListener('click', (event) => {
+      event.preventDefault();
+      aLinks.forEach((a) => a.classList.remove('active'));
+      document.body.style.backgroundImage = `url(${
+        images[event.target.dataset.link].image
+      })`;
+      navbar.style.backgroundColor = `${
+        images[event.target.dataset.link].color
+      }`;
+      event.target.classList.add('active');
+      router.draw(`${event.target.dataset.link}`);
+    });
   }
 }
 
