1ec9da804fc7c42e4a3e74f89027d95d451ac6c0
KotdenGit <kotden09@gmail.com>
2020-06-24 18:01:36 +0300

    feat: add start page game

--- src/components/phrase-wizard-page/StartGamePage.js
+++ src/components/phrase-wizard-page/StartGamePage.js
@@ -1,34 +1,6 @@
-import { createElement } from 'helpers/dom';
-import StartGame from 'components/phrase-wizard-page/StartGamePage';
-
-export default class PhraseWizard {
-  init() {
-    const gameBox = createElement(
-      document.getElementById('main-container'),
-      'contaner',
-      ['box-games']
-      );
-
-    createElement(gameBox, 'div', ['spinner']);
-    createElement(
-      document.querySelector('.spinner'),
-      'div',
-      ['spinner-border', 'text-warning'],
-      { role: 'status' }
-    );
-    createElement(
-      document.querySelector('.spinner-border'),
-      'span',
-      ['sr-only'],
-      {},
-      'Loading...'
-    );
-
-    createElement(gameBox, 'div', ['header']);
-    createElement(gameBox, 'div', ['wrapper']);
-    createElement(document.querySelector('.wrapper'), 'div', ['answerBlock']);
-
-    const intro = document.createElement('div');
+export default class StartGame {
+    static render() {
+        const intro = document.createElement('div');
         intro.classList.add('intro');
         intro.innerHTML = `
          <div class="title">
@@ -49,8 +21,12 @@ export default class PhraseWizard {
             <button type="button" class="btn btn-primary start learn">изучаемые слова</button>
          </div>
         `;
-        gameBox.append(intro);
-
-    return gameBox;
-  } 
+        document.body.append(intro);
+        
+        
+      }
+    
+      static init() {
+        this.render();
+      }
 }
