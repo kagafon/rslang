e93191712a1d9074d652829fc705e1238c50254f
furrrmanov <pokerzatashil@mail.ru>
2020-06-29 15:04:52 +0300

    feat: delete popap-error.js, index.js

--- src/components/games-AudioCall/app/app.js
+++ src/components/games-AudioCall/app/app.js
@@ -1,6 +1,4 @@
-// eslint-disable-next-line no-restricted-imports
 import { createElement } from 'helpers/dom';
-// eslint-disable-next-line import/no-cycle
 import StartPage from 'components/games-AudioCall/app/components/main/start-page/start-page';
 
 export default class App {
