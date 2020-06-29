1dd2b87b05f55b4e0dc20e16dd53d1499f9bafe0
KotdenGit <kotden09@gmail.com>
2020-06-29 17:00:03 +0300

    feat: add implementation of response mapping

--- src/components/phrase-wizard-page/app/words.js
+++ src/components/phrase-wizard-page/app/words.js
@@ -18,22 +18,53 @@ export default class GameWords {
     const wrapper = document.querySelector('.wrapper');
     const wordsBlock = createElement(wrapper, 'div', ['wordsblock']);
     let phraseRound = word.textExample.replace(/[/]/, "");
+
     phraseRound = phraseRound.replace(/<b>/g, "");
     phraseRound.substring(phraseRound.length - 1, phraseRound.length) == "." ? phraseRound = phraseRound.substring(0, phraseRound.length - 1) : true;
     const wordsArray = phraseRound.split(' ');
+
     const imagePhrase = document.createElement("img");
     imagePhrase.src = word.imageSrc;
     imagePhrase.alt = word.word;
     wrapper.append(imagePhrase);
-    //createElement(wrapper, 'img', [''], {src = word.image, alt = word.word});
 
     wordsArray.forEach(template => {
       this.template = template.replace(/[a-zA-Z]/g, "•");
       createElement(wordsBlock, 'span', ['temlate-words'], {style: 'color: black'}, this.template);
 
     });
-    
     wrapper.append(wordsBlock);
+    this.game(wordsArray);
+    console.log(wordsArray);
+  }
+
+  static game(gameWords) {
+    let correctWord = 0;
+    let incorrectWord = 0;
+    let numberWords = 0;
+    const wordsBlock = document.querySelector('.wordsblock');
+
+    wordCheck();
+    function wordCheck() {
+      document.addEventListener('keydown', function(event) {
+        if (gameWords.length > numberWords && event.code == `Key${gameWords[numberWords].charAt(0).toUpperCase()}`) {
+          wordsBlock.children[numberWords].innerText = '';
+          wordsBlock.children[numberWords].innerText = gameWords[numberWords];
+          wordsBlock.children[numberWords].classList.add('correct');
+          ++numberWords;
+          ++correctWord;
+        }else if(gameWords.length > numberWords){
+          wordsBlock.children[numberWords].innerText = '';
+          wordsBlock.children[numberWords].innerText = gameWords[numberWords];
+          wordsBlock.children[numberWords].classList.add('incorrect'); 
+          ++numberWords;
+          ++incorrectWord;
+        }else {
+          return
+        }
+      });  
+    } 
+    
   }
 
   static rightChoice(item) {
