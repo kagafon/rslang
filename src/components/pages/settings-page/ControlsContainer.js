786443c23bcc711cee880ec439e593f269a946fb
kagafon <kagafon@users.noreply.github.com>
2020-07-04 01:52:42 +0300

    fix: add setting check for translation in word details window, fix control-area css class

--- src/components/pages/settings-page/ControlsContainer.js
+++ src/components/pages/settings-page/ControlsContainer.js
@@ -4,13 +4,13 @@ import BaseControl from './BaseControl';
 export default class ControlsContainer extends BaseControl {
   constructor({ label, items }) {
     super();
-    this.container = createElement(null, 'div', ['settings-block']);
+    this.container = createElement(null, 'div', ['control-area']);
     const blockHeader = createElement(this.container, 'div', [
-      'settings-block-header',
+      'control-area-header',
     ]);
     createElement(blockHeader, 'h5', ['h5'], {}, label);
     const blockBody = createElement(this.container, 'div', [
-      'settings-block-body',
+      'control-area-body',
     ]);
 
     items.map((item) => new item.ClassName(item).render(blockBody));
