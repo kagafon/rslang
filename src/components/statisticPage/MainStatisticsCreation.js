4da433acef773b8648a874b579bbbe6dc315710c
Raman Liaukovich <2038634@gmail.com>
2020-07-09 05:44:39 +0300

    fix: fix bugs

--- src/components/statisticPage/MainStatisticsCreation.js
+++ src/components/statisticPage/MainStatisticsCreation.js
@@ -10,46 +10,68 @@ class MainStatisticsCreation {
     const newWordData = [];
     async function getMainStat() {
       try {
+        // save
+        // const stat = await User.getMainStatistics();
+        // stat.passedCards = stat.passedCards || 0;
+        // stat.correctAnswers = stat.correctAnswers || 0;
+        // stat.learnedWords = stat.learnedWords || 0;
+        // stat.answerSeries = stat.answerSeries || 0;
+        // stat.answerCount = stat.answerCount || 0;
+        // stat.passedCards = 33;
+        // stat.correctAnswers = 33;
+        // stat.learnedWords = 7;
+        // stat.answerSeries = 15;
+        // stat.answerCount = 12;
+        // User.saveMainStatistics(stat);
+        //
         const response = await User.getMainStatistics(true);
         console.log(response);
         const todayStatistic = response[response.length - 1];
         document.getElementById('today-passedCards').innerText =
-          todayStatistic.passedCards;
+          todayStatistic.passedCards || 0;
         document.getElementById('today-bestSeria').innerText =
-          todayStatistic.answerSeries;
+          todayStatistic.answerSeries || 0;
         document.getElementById('today-rightAnswers').innerText =
-          todayStatistic.correctAnswers;
+          todayStatistic.correctAnswers || 0;
         document.getElementById('today-newWords').innerText =
-          todayStatistic.learnedWords;
+          todayStatistic.learnedWords || 0;
         let dateFirst = new Date(today);
-        if (response.length < 8) {
+        const firstDate = response[0].d * 3600 * 1000 * 24;
+        const period = Math.round((today - firstDate) / 86400000);
+        console.log(period);
+        if (period < 8) {
           for (let i = 0; i < 7; i += 1) {
             dateFirst = new Date(today - (6 - i) * 86400000);
             labels[i] = new Date(dateFirst);
           }
           labels[6] = new Date(today);
         } else {
-          const firstDate = response[0].d * 3600 * 1000 * 24;
-          newWordData[0] = response[0].learnedWords || 0;
-          mainData[0] = newWordData[0];
           labels[0] = new Date(firstDate);
-          const period = Math.round((today - firstDate) / 86400000);
           let date = new Date(firstDate);
           for (let i = 0; i < period; i += 1) {
             date = new Date(date.setDate(date.getDate() + 1));
             labels[i + 1] = new Date(date);
           }
         }
-        console.log(labels)
         arr = labels.map((e) => e.toLocaleDateString('ru'));
         response.map(
-          (e) => (e.d = new Date(e.d).toLocaleString('ru').substring(0, 10))
+          (e) => (e.d = new Date(e.d * 3600 * 1000 * 24).toLocaleString('ru').substring(0, 10))
         );
-        console.log(arr);
+        const firstPoint = response.filter((e) => e.d === arr[0]);
+        if(firstPoint.length === 0 || firstPoint[0].learnedWords === undefined) {
+          mainData[0] = 0;
+        } else {
+          mainData[0] = firstPoint[0].learnedWords;
+        }
+        newWordData[0] = mainData[0];
         for (let i = 1; i < arr.length; i += 1) {
           const dayStat = response.filter((e) => e.d === arr[i]);
-          console.log(dayStat);
+          
           if (dayStat.length === 0 || dayStat[0].learnedWords === undefined) {
+            if(i === 0) {
+              mainData[i] = dayStat[0].learnedWords || 0;
+              newWordData[i] = mainData[i];
+            }
             newWordData[i] = 0;
             mainData[i] = mainData[i - 1];
           } else {
@@ -57,6 +79,8 @@ class MainStatisticsCreation {
             mainData[i] = mainData[i - 1] + newWordData[i];
           }
         }
+        console.log(newWordData);
+        console.log(mainData);
         MainChart.create(popCanvas, arr, mainData, newWordData);
       } catch (err) {
         console.log(err);
