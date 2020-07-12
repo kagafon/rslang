4aee604ece9056986df6628d813d4a2191ea9466
KotdenGit <kotden09@gmail.com>
2020-07-12 22:18:20 +0300

    feat: add transition audio interruption

--- src/components/phrase-wizard-page/app/button.js
+++ src/components/phrase-wizard-page/app/button.js
@@ -2,19 +2,20 @@ import { createElement } from 'helpers/dom';
 import GameWords from 'components/phrase-wizard-page/app/words';
 import Service from 'components/phrase-wizard-page/app/service';
 import Results from 'components/phrase-wizard-page/app/results';
+import Voice from 'components/phrase-wizard-page/app/voice';
 
 export default class Button {
   static render() {
     const btnBlock = document.querySelector('.top-block.ph-wiz');
     createElement(btnBlock, 'button', ['btn', 'btn-primary', 'ph-wiz'], {}, 'Дальше');
-    
   }
 
   static btnClick() {
     const nextButton = document.querySelector('.btn-primary.ph-wiz');
-    nextButton.addEventListener('click', () => {
-      ++GameWords.round;
+    nextButton.addEventListener('click', () => { 
       Service.spinnerOn();
+      Voice.playAudio.pause();
+      ++GameWords.round;
       document.removeEventListener('keydown', GameWords.keyListener);
       document.querySelector('.top-block.ph-wiz').textContent = '';
       document.querySelector('.wordsblock').remove();
