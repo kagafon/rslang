afd2b63f8e1a0bcc5995c61cae1de251fe21c62d
Raman Liaukovich <2038634@gmail.com>
2020-07-12 19:25:20 +0300

    fix: remove unnecessery code

--- src/components/promo-page/promo-page.js
+++ src/components/promo-page/promo-page.js
@@ -7,146 +7,6 @@ import optionsForSwiper from './optionsForSwiper';
 export default class promoPage {
   init() {
     this.parent = createElement(null, 'div', ['parent', 'promo-page'], {}, '');
-    // this.swiperContainer = createElement(
-    //   this.parent,
-    //   'div',
-    //   ['swiper-container'],
-    //   {},
-    //   ''
-    // );
-    // this.swiperWrapper = createElement(
-    //   this.swiperContainer,
-    //   'div',
-    //   ['swiper-wrapper'],
-    //   {},
-    //   ''
-    // );
-    // const item1 = createElement(
-    //   this.swiperWrapper,
-    //   'img',
-    //   ['swiper-slide'],
-    //   {
-    //     src: 'assets/images/promo-page/29d246cc8954f12113b3b86b0051464e.jpg',
-    //   },
-    //   ''
-    // );
-    // const item2 = createElement(
-    //   this.swiperWrapper,
-    //   'img',
-    //   ['swiper-slide'],
-    //   {
-    //     src: 'assets/images/promo-page/0_ef4fa_320c2e95_orig-640x360.jpg',
-    //   },
-    //   ''
-    // );
-    // const item3 = createElement(
-    //   this.swiperWrapper,
-    //   'img',
-    //   ['swiper-slide'],
-    //   {
-    //     src: 'assets/images/promo-page/1519728910159193202.jpg',
-    //   },
-    //   ''
-    // );
-    // const item4 = createElement(
-    //   this.swiperWrapper,
-    //   'img',
-    //   ['swiper-slide'],
-    //   {
-    //     src: 'assets/images/promo-page/3643a446463165.585530c44d950.jpg',
-    //   },
-    //   ''
-    // );
-    // const item5 = createElement(
-    //   this.swiperWrapper,
-    //   'img',
-    //   ['swiper-slide'],
-    //   {
-    //     src: 'assets/images/promo-page/57d92f3657b3a.jpg',
-    //   },
-    //   ''
-    // );
-    // const item6 = createElement(
-    //   this.swiperWrapper,
-    //   'img',
-    //   ['swiper-slide'],
-    //   {
-    //     src: 'assets/images/promo-page/priroda-gory-utki-ozero-peizazh.jpg',
-    //   },
-    //   ''
-    // );
-    // const item7 = createElement(
-    //   this.swiperWrapper,
-    //   'img',
-    //   ['swiper-slide'],
-    //   {
-    //     src: 'assets/images/promo-page/russia-flag-button-square-xs.png',
-    //   },
-    //   ''
-    // );
-    // const item8 = createElement(
-    //   this.swiperWrapper,
-    //   'img',
-    //   ['swiper-slide'],
-    //   {
-    //     src:
-    //       'assets/images/promo-page/united-states-of-america-flag-button-square-xs.png',
-    //   },
-    //   ''
-    // );
-    // const item9 = createElement(
-    //   this.swiperWrapper,
-    //   'img',
-    //   ['swiper-slide'],
-    //   {
-    //     src:
-    //       'assets/images/promo-page/viaduk-poezd-transport-zvezdy-liudi-peizazh-minimalizm-gory.jpg',
-    //   },
-    //   ''
-    // );
-    // const item10 = createElement(
-    //   this.swiperWrapper,
-    //   'video',
-    //   ['swiper-slide'],
-    //   {
-    //     style: 'width: 400; height: 300',
-    //     controls: 'controls',
-    //     preload: 'true',
-    //     poster: 'assets/images/russia-flag-button-square-xs.png',
-    //   },
-    //   ''
-    // );
-    // const source = createElement(
-    //   item10,
-    //   'source',
-    //   ['source'],
-    //   {
-    //     src: 'assets/images/promo-page/videoplayback.mp4',
-    //     type: 'video/mp4;',
-    //   },
-    //   ''
-    // );
-    // const btnPrev = createElement(
-    //   this.swiperContainer,
-    //   'div',
-    //   ['swiper-button-prev'],
-    //   {},
-    //   ''
-    // );
-    // const btnNext = createElement(
-    //   this.swiperContainer,
-    //   'div',
-    //   ['swiper-button-next'],
-    //   {},
-    //   ''
-    // );
-    // const headerPromo = createElement(
-    //   this.parent,
-    //   'div',
-    //   ['promo-header'],
-    //   {},
-    //   ''
-    // );
 
     const headerPromoVideo = createElement(
       this.parent,
@@ -569,9 +429,6 @@ export default class promoPage {
       {},
       'Дано слово и перевод, а также два варианта ответа - верно или нет. Игрок выбирает один из них.'
     );
-    // this.swiper = new Swiper(this.swiperContainer, optionsForSwiper);
-    // this.slideNext(btnNext);
-    // this.slidePrev(btnPrev);
     GetStartButton.addEventListener('click', () => {
       Router.draw('authorization-page');
     });
@@ -596,21 +453,4 @@ export default class promoPage {
 
     return this.parent;
   }
-
-
-  // postInit() {
-  //   this.swiper.update();
-  // }
-
-  // slidePrev(btn) {
-  //   btn.addEventListener('click', () => {
-  //     this.swiper.slidePrev();
-  //   });
-  // }
-
-  // slideNext(button) {
-  //   button.addEventListener('click', () => {
-  //     this.swiper.slideNext();
-  //   });
-  // }
 }
